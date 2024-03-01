a3a52d3a90369566878df1388f2bb8b466eb0b70
Stacey Graham <737745-StaceyGraham@users.noreply.replit.com>
2024-03-01 22:49:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print("My Custom Affirmations Generator")
+print()
+yourName = input("What is your name? ")
+print("Hi! " + yourName + " 😀")
+print()
+dayOfTheWeek = input("What is the day of the week? ")
+print()
+favThing = input("What is your favorite thing to do? ")
+print()
+favDrink = input("What is your favorite drink? ")
+print()
+if dayOfTheWeek == "saturday" or dayOfTheWeek == "Saturday":
+  print("Hey ", yourName, ",", "this ",   dayOfTheWeek, " get your ", favDrink, " do ", favThing, " and enjoy yourself and remember you are AWESOME and you can do anything you put your mind to. Go be GREAT!")
+elif dayOfTheWeek == "sunday" or dayOfTheWeek == "Sunday":
+    print("Rest", dayOfTheWeek, " and tomorrow start off the week well rested with your ", favDrink, " and seize the day!!")
+elif dayOfTheWeek == "Monday" or dayOfTheWeek == "monday":
+    print("It's", dayOfTheWeek, " It's a great day! Keep shining bright ☀️")
+elif dayOfTheWeek == "Wednesday" or dayOfTheWeek == "wednesday":
+    print("It's", dayOfTheWeek, " Hump day! Week is halfway through! Press through. You got this 💪🏾")
+else:
+    print("You are going to have an Amazing week! Remember you can do hard things!")
+
+
+
